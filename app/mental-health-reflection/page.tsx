c5167411efa19eaf76035f308f502ffd9bffa991
mehul <github@mehapps.com>
2025-02-08 22:05:09 -0500

    Change reflection question format
    Fixes #4

--- app/mental-health-reflection/page.tsx
+++ app/mental-health-reflection/page.tsx
@@ -1,36 +1,53 @@
-// Don't need to hardcode once we get translation support
 import { questions } from "./questions"
 
 export default function Page() {
   const renderedQuestions = questions.map((question, idx) => {
-    let opts = question.options.map((opt, o_idx) => (
-      <div key={o_idx} className="flex flex-row my-2">
-        <input name={`Question_${idx}`} className="form-radio size-6 accent-blue-500" type="radio" id={`${idx}_${o_idx}`} />
-        <label className="text-black pl-2" htmlFor={`${idx}_${o_idx}`}>{opt.text}</label>
+    const opts = question.options.map((opt, o_idx) => (
+      <div key={o_idx} className="flex items-center mb-4 text-gray-900 hover:text-gray-500 focus:ring-2 focus:ring-blue-300">
+        <input
+          id={`question_${idx}_option_${o_idx}`}
+          type="radio"
+          name={`Question_${idx}`}
+          value={opt.text}
+          className="w-4 h-4 border-gray-300 accent-blue-500"
+        />
+        <label
+          htmlFor={`question_${idx}_option_${o_idx}`}
+          className="block ml-2 text-sm font-medium"
+        >
+          {opt.text}
+        </label>
       </div>
     ));
 
     return (
-      <fieldset className="w-[60%] bg-white h-full p-4 rounded-xl border-gray-300 border-2" key={idx}>
-        <legend className="text-black text-lg px-2">{question.question}</legend>
-
+      <fieldset
+        key={idx}
+        className="block w-full p-6 bg-white border border-blue-300 rounded-xl shadow-sm"
+      >
+        <legend className="sr-only">{question.question}</legend>
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-black">
+          {question.question}
+        </h5>
         {opts}
       </fieldset>
     );
   });
 
   return (
-    <div className="relative flex flex-col items-center gap-8 w-3/5 overflow-y-scroll">
-      <h1 className="text-blue-500 font-extrabold text-[46px] leading-[1]">
+    <div className="relative flex flex-col items-center gap-8 w-1/2 overflow-y-scroll">
+      <h1 className="text-blue-500 font-extrabold text-[46px] leading-[1] mb-6">
         Reflect on your mental health.
       </h1>
 
       {renderedQuestions}
-
-      <button
-        className="flex flex-row items-center justify-center gap-2 bg-blue-500/25 hover:bg-blue-600/25 px-7 py-3 my-4 rounded-lg border border-blue-600 active:bg-blue-500/35 duration-50">
-        <p className="antialiased text-lg text-blue-600">Get Your Activity Reccomendation</p>
-      </button>
+      <div className="mb-6">
+        <button className="bg-blue-500/25 hover:bg-blue-600/25 px-7 py-3 my-4 rounded-lg border border-blue-600 active:bg-blue-500/35 duration-50 mb-6">
+          <p className="antialiased text-lg text-blue-600">
+            Get Your Activity Recommendation
+          </p>
+        </button>
+      </div>
     </div>
   )
 }
